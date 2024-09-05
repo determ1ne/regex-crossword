d0699a8990769611920746360c642c5df64b86f2
Azuk 443 <me@azuk.top>
2024-09-05 13:55:34 +0800

    refactor state management add reverse highlight fix dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,23 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { createContext, useMemo, useState } from "react";
 import "./App.css";
-import { useMemo } from "react";
+import { useContext } from "react";
 
-const isAlpha = (str) => {
-  if (str.length !== 1) return false;
-  const code = str.charCodeAt(0);
-  return (code > 64 && code < 91) || (code > 96 && code < 123);
-};
+// *******************************
+// Regex List
+// *******************************
 
-const Hex = ({ word, setWordCallback, highlighted }) => {
-  const [isActive, setIsActive] = useState(false);
-
-  const activeClass = isActive ? "active" : "";
-  const highlightedClass = `highlighted-${highlighted}`;
-  const wordInInput = isActive ? "" : word;
-
-  return (
-    <div className="hex">
-      <div className="top"></div>
-      <div className="middle">
-        <div className="hex-inner">
-          <div className={`top ${activeClass} ${highlightedClass}`}></div>
-          <div className={`middle ${activeClass} ${highlightedClass}`}>
-            <input
-              className="hex-input"
-              onFocus={() => setIsActive(true)}
-              onBlur={() => setIsActive(false)}
-              value={wordInInput || ""}
-              onChange={(e) => {
-                const v = e.target.value.toUpperCase();
-                if (isAlpha(v)) {
-                  setWordCallback(v);
-                } else {
-                  setWordCallback(null);
-                }
-                e.target.blur();
-              }}
-            ></input>
-          </div>
-          <div className={`bottom ${activeClass} ${highlightedClass}`}></div>
-        </div>
-      </div>
-      <div className="bottom"></div>
-    </div>
-  );
-};
-
-const HexRow = ({
-  itemCount,
-  offset,
-  wordLine,
-  callback,
-  isHexHighlightedForRow,
-}) => {
-  const setWordForIndex = (j) => {
-    return (word) => {
-      callback(j, word);
-    };
-  };
-
-  return (
-    <div
-      className="hex-row"
-      style={{
-        paddingLeft: offset ? `${offset * 53}px` : 0,
-      }}
-    >
-      {Array.from({ length: itemCount }).map((_, index) => (
-        <Hex
-          key={index}
-          word={wordLine[index]}
-          setWordCallback={setWordForIndex(index)}
-          highlighted={isHexHighlightedForRow(index)}
-        />
-      ))}
-    </div>
-  );
-};
-
-const baseOffset = 6;
-
+// Direction
+// i: Row, j: Column
+// P: Positive, N: Negative
 const D_JP = 0;
 // const D_JN = 1;
 const D_IP = 2;
 const D_IN = 3;
 
+// prettier-ignore
 const _regexList = [
-  //
+  // j positive
   { content: ".*H.*H.*", extractor: { i: 0, j: 0, d: D_JP } },
   { content: "(DI|NS|TH|OM)*", extractor: { i: 1, j: 0, d: D_JP } },
   { content: "F.*[AO].*[AO].*", extractor: { i: 2, j: 0, d: D_JP } },
@@ -101,7 +31,7 @@ const _regexList = [
   { content: "N.*X.X.X.*E", extractor: { i: 10, j: 0, d: D_JP } },
   { content: "R*D*M*", extractor: { i: 11, j: 0, d: D_JP } },
   { content: ".(C|HH)*", extractor: { i: 12, j: 0, d: D_JP } },
-  //
+  // i positive left part
   { content: "(ND|ET|IN)[^X]*", extractor: { i: 0, j: 0, d: D_IP } },
   { content: "[CHMNOR]*I[CHMNOR]*", extractor: { i: 0, j: 1, d: D_IP } },
   { content: "P+(..)\\1.*", extractor: { i: 0, j: 2, d: D_IP } },
@@ -109,14 +39,14 @@ const _regexList = [
   { content: "([^MC]|MM|CC)*", extractor: { i: 0, j: 4, d: D_IP } },
   { content: "[AM]*CM(RC)*R?", extractor: { i: 0, j: 5, d: D_IP } },
   { content: ".*", extractor: { i: 0, j: 6, d: D_IP } },
-  //
+  // i positive right part
   { content: ".*PRR.*DDC.*", extractor: { i: 1, j: 7, d: D_IP } },
   { content: "(HHX|[^HX])*", extractor: { i: 2, j: 8, d: D_IP } },
   { content: "([^EMC]|EM)*", extractor: { i: 3, j: 9, d: D_IP } },
   { content: ".*OXR.*", extractor: { i: 4, j: 10, d: D_IP } },
   { content: ".*LR.*RL.*", extractor: { i: 5, j: 11, d: D_IP } },
   { content: ".*SE.*UE.*", extractor: { i: 6, j: 12, d: D_IP } },
-  //
+  // i negative right part
   { content: "(S|MM|HHH)*", extractor: { i: 6, j: 12, d: D_IN } },
   { content: "[^M]*M[^M]*", extractor: { i: 7, j: 11, d: D_IN } },
   { content: "(RX|[^R])*", extractor: { i: 8, j: 10, d: D_IN } },
@@ -124,23 +54,14 @@ const _regexList = [
   { content: ".*(.)C\\1X\\1.*", extractor: { i: 10, j: 8, d: D_IN } },
   { content: "[^C]*MMM[^C]*", extractor: { i: 11, j: 7, d: D_IN } },
   { content: ".*(IN|SE|HI)", extractor: { i: 12, j: 6, d: D_IN } },
-  //
-  {
-    content: ".*(.)(.)(.)(.)\\4\\3\\2\\1.*",
-    extractor: { i: 12, j: 5, d: D_IN },
-  },
+  // i negative left part
+  { content: ".*(.)(.)(.)(.)\\4\\3\\2\\1.*", extractor: { i: 12, j: 5, d: D_IN }, },
   { content: ".*XHCR.*X.*", extractor: { i: 12, j: 4, d: D_IN } },
   { content: ".*DD.*CCM.*", extractor: { i: 12, j: 3, d: D_IN } },
   { content: ".*XEXM*", extractor: { i: 12, j: 2, d: D_IN } },
   { content: "[CR]*", extractor: { i: 12, j: 1, d: D_IN } },
   { content: ".*G.*V.*H.*", extractor: { i: 12, j: 0, d: D_IN } },
 ];
-const regexList = _regexList.map((r) => {
-  return {
-    ...r,
-    regex: new RegExp("^" + r.content + "$", "i"),
-  };
-});
 
 const getBoardChar = (board, i, j) => {
   if (i < 0 || i > 12) return undefined;
@@ -149,11 +70,6 @@ const getBoardChar = (board, i, j) => {
   if (i > 6 && j > 18 - i) return undefined;
 
   return (board[i] ?? [])[j] ?? "_";
-};
-
-const testRegex = (regex, str) => {
-  if (str.indexOf("_") !== -1) return false;
-  return regex.test(str);
 };
 
 const getPath = (regexItem) => {
@@ -181,6 +97,38 @@ const getPath = (regexItem) => {
   }
 
   return path;
+};
+
+const regexList = _regexList
+  .map((r) => ({
+    ...r,
+    regex: new RegExp("^" + r.content + "$", "i"),
+  }))
+  .map((r) => ({
+    ...r,
+    path: getPath(r),
+  }));
+
+const hexToRegexList = (() => {
+  const res = [];
+  for (let regexId = 0; regexId < regexList.length; regexId++) {
+    for (let [i, j] of regexList[regexId].path) {
+      if (!res[i]) res[i] = [];
+      if (!res[i][j]) res[i][j] = [];
+      res[i][j].push(regexId);
+    }
+  }
+  return res;
+})();
+
+// Line status
+const LS_RIGHT = 0;
+const LS_WRONG = 1;
+const LS_INCOMPLETE = 2;
+
+const testRegex = (regex, str) => {
+  if (str.indexOf("_") !== -1) return LS_INCOMPLETE;
+  return regex.test(str) ? LS_RIGHT : LS_WRONG;
 };
 
 const checkRegex = (board, regexItem) => {
@@ -213,6 +161,98 @@ const checkRegex = (board, regexItem) => {
   throw new Error("Invalid direction");
 };
 
+// *******************************
+// App
+// *******************************
+
+const AppContext = createContext(null);
+
+// Hex: a hexagon cell
+const Hex = ({ cellIdx }) => {
+  const { activeHex, setActiveHex, board, setWordForIndex, highlightPath } =
+    useContext(AppContext);
+  const char = board[cellIdx[0]][cellIdx[1]];
+
+  const isActive =
+    activeHex && activeHex[0] === cellIdx[0] && activeHex[1] === cellIdx[1];
+  const highlightIndex = (highlightPath ?? []).findIndex(
+    (x) => x[0] === cellIdx[0] && x[1] === cellIdx[1]
+  );
+
+  const activeClass = isActive ? "active" : "";
+  const highlightedClass = `highlighted-${highlightIndex}`;
+  const wordInInput = isActive ? "" : char ?? "";
+
+  const setWord = (v) => {
+    if (v !== null) {
+      if (v.length !== 1) {
+        v = null;
+      } else {
+        const code = v.charCodeAt(0);
+        const isAlpha = (code > 64 && code < 91) || (code > 96 && code < 123);
+        v = isAlpha ? v : null;
+      }
+    }
+    setWordForIndex(cellIdx[0], cellIdx[1], v);
+  };
+
+  return (
+    <div className="hex">
+      <div className="top"></div>
+      <div className="middle">
+        <div className="hex-inner">
+          <div className={`top ${activeClass} ${highlightedClass}`}></div>
+          <div className={`middle ${activeClass} ${highlightedClass}`}>
+            <input
+              className="hex-input"
+              onFocus={() => setActiveHex(cellIdx)}
+              onBlur={() =>
+                setActiveHex((prev) => {
+                  if (prev[0] === cellIdx[0] && prev[1] === cellIdx[1])
+                    return null;
+                  return prev;
+                })
+              }
+              value={wordInInput || ""}
+              onChange={(e) => {
+                const v = e.target.value?.toUpperCase();
+                setWord(v);
+                e.target.blur();
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Backspace" || e.key === "Delete") {
+                  setWord(null);
+                  e.target.blur();
+                }
+              }}
+            ></input>
+          </div>
+          <div className={`bottom ${activeClass} ${highlightedClass}`}></div>
+        </div>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
+};
+
+// HexRow: a row of hexagons
+const HexRow = ({ itemCount, offset, rowIdx }) => {
+  return (
+    <div
+      className="hex-row"
+      style={{
+        paddingLeft: offset ? `${offset * 53}px` : 0,
+      }}
+    >
+      {Array.from({ length: itemCount }).map((_, index) => (
+        <Hex key={index} cellIdx={[rowIdx, index]} />
+      ))}
+    </div>
+  );
+};
+
+const baseOffset = 6;
+
 function App() {
   const [board, setBoard] = useState(() => {
     const savedData = localStorage.getItem("board");
@@ -224,8 +264,12 @@ function App() {
     }
     return board ?? Array.from({ length: 13 }).fill([]);
   });
+  // the regex has mouse hover, used to highlight hex line
   const [highLightItem, setHighLightItem] = useState(-1);
+  // the regex has mouse click
   const [stickHighlight, setStickHighlight] = useState(-1);
+  // the hex in edit mode
+  const [activeHex, setActiveHex] = useState(null);
 
   const setWordForIndex = (i, j, word) => {
     setBoard((prev) => {
@@ -238,36 +282,29 @@ function App() {
     });
   };
 
-  const setWordForIndexRowed = (i) => {
-    return (j, word) => {
-      setWordForIndex(i, j, word);
-    };
-  };
-
   const highlightPath = useMemo(() => {
     if (highLightItem === -1) {
       if (stickHighlight === -1) return [];
-      return getPath(regexList[stickHighlight]);
+      return regexList[stickHighlight]["path"];
     }
-    return getPath(regexList[highLightItem]);
+    return regexList[highLightItem]["path"];
   }, [highLightItem, stickHighlight]);
 
-  const isHexHighlighted = (i, j) => {
-    for (let index = 0; index < highlightPath.length; index++) {
-      const [x, y] = highlightPath[index];
-      if (x === i && y === j) {
-        return index;
-      }
-    }
-    return -1;
-  };
+  const activeRegexList = useMemo(() => {
+    if (activeHex === null) return [];
+    return hexToRegexList[activeHex[0]]?.[activeHex[1]] ?? [];
+  }, [activeHex]);
 
-  const isHexHighlightedForRow = (i) => {
-    return (j) => isHexHighlighted(i, j);
+  const contextValue = {
+    activeHex,
+    setActiveHex,
+    board,
+    setWordForIndex,
+    highlightPath,
   };
 
   return (
-    <>
+    <AppContext.Provider value={contextValue}>
       <div className="header">
         <h1>Regex Crossword Puzzle</h1>
         <div style={{ textAlign: "center" }}>
@@ -287,6 +324,7 @@ function App() {
                 try {
                   const newBoard = JSON.parse(bordJSON);
                   setBoard(newBoard);
+                  localStorage.setItem("board", JSON.stringify(newBoard));
                 } catch {
                   alert("Invalid JSON");
                 }
@@ -309,99 +347,24 @@ function App() {
           </button>
         </div>
       </div>
-      <form className="crossword-container">
-        <HexRow
-          itemCount={7}
-          offset={baseOffset}
-          wordLine={board[0]}
-          callback={setWordForIndexRowed(0)}
-          isHexHighlightedForRow={isHexHighlightedForRow(0)}
-        />
-        <HexRow
-          itemCount={8}
-          offset={baseOffset - 1}
-          wordLine={board[1]}
-          callback={setWordForIndexRowed(1)}
-          isHexHighlightedForRow={isHexHighlightedForRow(1)}
-        />
-        <HexRow
-          itemCount={9}
-          offset={baseOffset - 2}
-          wordLine={board[2]}
-          callback={setWordForIndexRowed(2)}
-          isHexHighlightedForRow={isHexHighlightedForRow(2)}
-        />
-        <HexRow
-          itemCount={10}
-          offset={baseOffset - 3}
-          wordLine={board[3]}
-          callback={setWordForIndexRowed(3)}
-          isHexHighlightedForRow={isHexHighlightedForRow(3)}
-        />
-        <HexRow
-          itemCount={11}
-          offset={baseOffset - 4}
-          wordLine={board[4]}
-          callback={setWordForIndexRowed(4)}
-          isHexHighlightedForRow={isHexHighlightedForRow(4)}
-        />
-        <HexRow
-          itemCount={12}
-          offset={baseOffset - 5}
-          wordLine={board[5]}
-          callback={setWordForIndexRowed(5)}
-          isHexHighlightedForRow={isHexHighlightedForRow(5)}
-        />
-        <HexRow
-          itemCount={13}
-          offset={baseOffset - 6}
-          wordLine={board[6]}
-          callback={setWordForIndexRowed(6)}
-          isHexHighlightedForRow={isHexHighlightedForRow(6)}
-        />
-        <HexRow
-          itemCount={12}
-          offset={baseOffset - 5}
-          wordLine={board[7]}
-          callback={setWordForIndexRowed(7)}
-          isHexHighlightedForRow={isHexHighlightedForRow(7)}
-        />
-        <HexRow
-          itemCount={11}
-          offset={baseOffset - 4}
-          wordLine={board[8]}
-          callback={setWordForIndexRowed(8)}
-          isHexHighlightedForRow={isHexHighlightedForRow(8)}
-        />
-        <HexRow
-          itemCount={10}
-          offset={baseOffset - 3}
-          wordLine={board[9]}
-          callback={setWordForIndexRowed(9)}
-          isHexHighlightedForRow={isHexHighlightedForRow(9)}
-        />
-        <HexRow
-          itemCount={9}
-          offset={baseOffset - 2}
-          wordLine={board[10]}
-          callback={setWordForIndexRowed(10)}
-          isHexHighlightedForRow={isHexHighlightedForRow(10)}
-        />
-        <HexRow
-          itemCount={8}
-          offset={baseOffset - 1}
-          wordLine={board[11]}
-          callback={setWordForIndexRowed(11)}
-          isHexHighlightedForRow={isHexHighlightedForRow(11)}
-        />
-        <HexRow
-          itemCount={7}
-          offset={baseOffset}
-          wordLine={board[12]}
-          callback={setWordForIndexRowed(12)}
-          isHexHighlightedForRow={isHexHighlightedForRow(12)}
-        />
-      </form>
+      <div className="crossword-container">
+        {Array.from({ length: 7 }).map((_, i) => (
+          <HexRow
+            key={i}
+            rowIdx={i}
+            itemCount={i + 7}
+            offset={baseOffset - i}
+          />
+        ))}
+        {Array.from({ length: 6 }).map((_, i) => (
+          <HexRow
+            key={i + 7}
+            rowIdx={i + 7}
+            itemCount={12 - i}
+            offset={baseOffset + i - 5}
+          />
+        ))}
+      </div>
       <h2>Regex List</h2>
       <div className="regex-list">
         {regexList.map((r, i) => {
@@ -410,7 +373,9 @@ function App() {
             <div
               key={i}
               className={`regex-item ${
-                highLightItem === i ? "regex-active" : ""
+                highLightItem === i || activeRegexList.indexOf(i) !== -1
+                  ? "regex-active"
+                  : ""
               }`}
               onClick={() => {
                 setStickHighlight((prev) => {
@@ -429,13 +394,21 @@ function App() {
               }}
             >
               <span className="regex-content">{r.content}</span>
-              <span className="regex-extracted">{str}</span>
-              <span className="regex-result">{result ? "✅" : "❌"}</span>
+              <span className="regex-extracted">
+                {Array.from(str).map((v, i) => (
+                  <span className="regex-input-char" key={i}>
+                    {v}
+                  </span>
+                ))}
+              </span>
+              <span className="regex-result">
+                {result === LS_RIGHT ? "✅" : result === LS_WRONG ? "❌" : "🚧"}
+              </span>
             </div>
           );
         })}
       </div>
-    </>
+    </AppContext.Provider>
   );
 }
 
